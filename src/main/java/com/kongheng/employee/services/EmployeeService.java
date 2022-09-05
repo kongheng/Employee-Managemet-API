77af950a51291a10ea6af81c7aedd7cbe6c75359
kongheng <kongheng.long14@gmail.com>
2022-09-06 06:46:07 +0700

    employee-api

---
+++ src/main/java/com/kongheng/employee/services/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.kongheng.employee.services;
+
+import com.kongheng.employee.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    Employee createEmployee(Employee employee);
+
+    List<Employee> getAllEmployees();
+
+    boolean deleteEmployee(Long id);
+
+    Employee getEmployeeById(Long id);
+
+    Employee updateEmployee(Long id, Employee employee);
+}
